c963e9f06ff49ae4adb3b333dc01fb58f7f960d4
Fred Malouf <fred.malouf@nio.io>
2020-10-22 10:43:54 -0700

    Add cpp_info.defines for QNX

--- conanfile.py
+++ conanfile.py
@@ -90,3 +90,4 @@ class CapicxxSomeipRuntimeConan(ConanFile):
             self.cpp_info.libs.extend(['pthread'])
         elif self.settings.os == "QNX":
             self.cpp_info.libs.extend(['socket'])
+            self.cpp_info.defines.extend(["__EXT_BSD", "__QNXNTO__", "_QNX_SOURCE"])
